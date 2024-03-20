db9a17996750452653be005aa8ea2b97fc2db41c
danisachk <65428342+danisachk@users.noreply.github.com>
2024-03-20 00:47:30 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -288,3 +288,12 @@ document.addEventListener("DOMContentLoaded", function() {
     //     languageSwitcher.classList.add("switched");
     // }
 });
+
+// //---------------------------------------------------------------------------------
+// // CONTRAER NAVBAR COLLAPSE
+
+$(document).ready(function () {
+    $(".navbar-nav .nav-link").on("click", function () {
+      $(".navbar-collapse").collapse("hide");
+    });
+});
